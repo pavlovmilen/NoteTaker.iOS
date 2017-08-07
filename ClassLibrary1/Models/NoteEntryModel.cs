b102d1acc7a2bbec1779e6de68f1b9aeb41171b0
Milen Pavlov <milenppavlov@gmail.com>
2017-08-07 20:40:03 +0100

    Add project files.

---
+++ ClassLibrary1/Models/NoteEntryModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteTaker.Core.Models
+{
+    public class NoteEntryModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime?  CompletedOn { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+    }
+}
---
+++ NoteTaker.iOS/DetailViewController.designer.cs
@@ -0,0 +1,37 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace NoteTaker.iOS
+{
+    [Register ("DetailViewController")]
+    partial class DetailViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextView NoteDesctiptionTextView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField TitleTextField { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (NoteDesctiptionTextView != null) {
+                NoteDesctiptionTextView.Dispose ();
+                NoteDesctiptionTextView = null;
+            }
+
+            if (TitleTextField != null) {
+                TitleTextField.Dispose ();
+                TitleTextField = null;
+            }
+        }
+    }
+}
